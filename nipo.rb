b9863fb0834e39eb0749b8b53cc377a353cefe30
endu <futoshi.endo@pepabo.com>
2018-03-07 12:24:31 +0900

    CLIからscrapboxのnipoを立ち上げるスクリプトを作成した。

---
+++ nipo.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+require 'uri'
+
+project = 'fendo181'
+year  = Time.now.strftime('%Y')
+month = Time.now.strftime('%m')
+day   = Time.now.strftime('%d')
+timestamp = ["#{year}", "%2F", "#{month}", "%2F", "#{day}"].join
+uri = "https://scrapbox.io/#{project}/#{timestamp}"
+system "open #{uri}"

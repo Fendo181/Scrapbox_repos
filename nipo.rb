9a4c9d534f30d10e4508f4b8f43cda756578bad4
endu <futoshi.endo@pepabo.com>
2018-03-07 14:16:25 +0900

    :sparkles: タイムスタンプ部分を改善した・

--- nipo.rb
+++ nipo.rb
@@ -6,5 +6,6 @@ year  = Time.now.strftime('%Y')
 month = Time.now.strftime('%m')
 day   = Time.now.strftime('%d')
 timestamp = ["#{year}", "%2F", "#{month}", "%2F", "#{day}"].join
+
 uri = "https://scrapbox.io/#{project}/#{timestamp}"
 system "open #{uri}"
